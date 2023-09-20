8df986d1398c8971483efa1340a1de5a31ff95ae
Matheus Loureiro <loureiro58@gmail.com>
2023-09-19 22:30:41 -0300

    Atualizações conforme aula DIO sobre padrões de projeto

--- src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -1,8 +1,23 @@
 package one.digitalinnovation.gof;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ *	Projeto Spring Boot gerado via Spring Initializr
+ *	Os seguintes starters foram selecionados:
+ *	 - Spring Data JPA
+ *	 - Spring Web
+ *	 - H2 Database
+ *	 - OpenFeign
+ *
+ *	 @Autor loureiro58
+ */
+
+@EnableFeignClients
 @SpringBootApplication
 public class LabPadroesProjetoSpringApplication {
 
